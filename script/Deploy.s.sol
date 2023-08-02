b9c47481ef94879e3df6cd6cdd0ea47ee8c5160b
lilPlumberBoy <dalymike102@gmail.com>
2023-08-02 16:31:15 -0400

    fix: added comments to example deploy script

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -2,13 +2,22 @@
 pragma solidity ^0.8.13;
 
 import "forge-deploy/DeployScript.sol";
+// generated from looking at contracts with ./forge-deploy gen-deployer
 import "generated/deployer/DeployerFunctions.g.sol";
+// Could also import the default deployer functions
+// import "forge-deploy/DefaultDeployerFunction.sol";
 
 contract Deployments is DeployScript {
+    // Using generated functions
     using DeployerFunctions for Deployer;
+    // Using default deployer function
+    using DefaultDeployerFunction for Deployer;
 
     function deploy() external returns (Counter) {
+        // Using generated function with name deploy_<contract_name>
         return deployer.deploy_Counter("Counter");
+        // Using default deployer function
+        // return Counter(deployer.deploy("Counter", "Counter.sol:Counter", ""));
     }
 }
 // DEPLOYMENT_CONTEXT=localhost forge script script/Counter.s.sol --rpc-url http://localhost:8545 --broadcast --private-key ac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80 -v && ./forge-deploy sync;
