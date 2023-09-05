21bd472490ee05b7742bee72de60e9d22bb943c4
Jongseung Lim <penandlim@gmail.com>
2023-09-05 12:57:07 -0400

    chore: install veYFI@master

--- src/YearnStakingDelegate.sol
+++ src/YearnStakingDelegate.sol
@@ -1,11 +1,15 @@
 // SPDX-License-Identifier: UNLICENSED
-
 pragma solidity 0.8.18;
+
+import { SafeERC20, IERC20 } from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
 contract YearnStakingDelegate {
     /// @notice Mapping of vault to gauge
     mapping(address vault => address gauge) public associatedGauge;
     address public manager;
+    address public oYFI;
+
+    using SafeERC20 for IERC20;
 
     modifier onlyManager() {
         require(msg.sender == manager, "Only manager can call this function.");
@@ -16,12 +20,12 @@ contract YearnStakingDelegate {
         strategy = msg.sender;
         associatedGauge[vault].claim();
         // Do actions based on configured parameters
-        oYFI.transfer(treasury, treasuryAmount);
-        oYFI.transfer(strategy, compoundAmount);
+        IERC20(oYFI).transfer(treasury, treasuryAmount);
+        IERC20(oYFI).transfer(strategy, compoundAmount);
     }
 
     function depositAndStake(address vault, uint256 amount) external {
-        vault.transferFrom(msg.sender, amount, here);
+        IERC20(vault).transferFrom(msg.sender, amount, address(this));
         associatedGauge[vault].deposit(amount);
         balances[msg.sender][vault] += amount;
     }
