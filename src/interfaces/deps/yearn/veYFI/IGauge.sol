78167993c41f1befb63865b6023b8d2bfa96e771
Jongseung Lim <penandlim@gmail.com>
2023-10-12 14:18:51 -0400

    chore: change interface import

--- src/interfaces/deps/yearn/veYFI/IGauge.sol
+++ src/interfaces/deps/yearn/veYFI/IGauge.sol
@@ -2,9 +2,9 @@
 pragma solidity ^0.8.20;
 
 import { IBaseGauge } from "./IBaseGauge.sol";
-import { IERC4626Upgradeable } from "@openzeppelin-upgradeable-5.0/contracts/interfaces/IERC4626Upgradeable.sol";
+import { IERC4626 } from "@openzeppelin-5.0/contracts/interfaces/IERC4626.sol";
 
-interface IGauge is IBaseGauge, IERC4626Upgradeable {
+interface IGauge is IBaseGauge, IERC4626 {
     function initialize(address _stakingToken, address _owner) external;
     function boostedBalanceOf(address _account) external view returns (uint256);
     function getReward(address _account) external returns (bool);
