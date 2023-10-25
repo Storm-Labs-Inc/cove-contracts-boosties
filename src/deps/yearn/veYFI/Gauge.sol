ee71ac51f5cda8007b7f1114f83c60df200bd155
Sunil Srivatsa <sunilsrivatsa@gmail.com>
2023-10-25 14:33:54 -0400

    fix: update pragmas to 0.8.20

--- src/deps/yearn/veYFI/Gauge.sol
+++ src/deps/yearn/veYFI/Gauge.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.15;
+pragma solidity ^0.8.20;
 
 import "./interfaces/IExtraReward.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
