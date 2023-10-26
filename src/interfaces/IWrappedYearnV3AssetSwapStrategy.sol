12721725aede4a30133a2d28699067ea04fa2cba
lilPlumberBoy <dalymike102@gmail.com>
2023-10-26 16:11:01 -0400

    fix: updated based on solc version change

--- src/interfaces/IWrappedYearnV3AssetSwapStrategy.sol
+++ src/interfaces/IWrappedYearnV3AssetSwapStrategy.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.20;
+pragma solidity >=0.8.18;
 
 import { CurveRouterSwapper } from "src/swappers/CurveRouterSwapper.sol";
 import { IWrappedYearnV3Strategy } from "src/interfaces/IWrappedYearnV3Strategy.sol";
@@ -8,7 +8,6 @@ import { StrategyAssetSwap } from "src/strategies/StrategyAssetSwap.sol";
 interface IWrappedYearnV3AssetSwapStrategy is IWrappedYearnV3Strategy {
     // Need to override the `asset` function since
     // its defined in both interfaces inherited.
-    function asset() external view override(IWrappedYearnV3Strategy) returns (address);
     function setOracle(address token, address oracle) external;
     function setSwapParameters(
         CurveRouterSwapper.CurveSwapParams memory deploySwapParams,
