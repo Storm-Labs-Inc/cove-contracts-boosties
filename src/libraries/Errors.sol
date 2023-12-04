fed380146fa93f31c76182b060e9351728edcf28
Jongseung Lim <penandlim@gmail.com>
2023-12-04 12:00:48 -0500

    Revert "chore: clean up StakingDelegateRewards.sol"
    This reverts commit 1568af8d287dd1c12383a9178c0b33fedfcdd4ec.

--- src/libraries/Errors.sol
+++ src/libraries/Errors.sol
@@ -56,7 +56,6 @@ library Errors {
     error OnlyStakingDelegateCanUpdateUserBalance();
     error OnlyStakingDelegateCanAddStakingToken();
     error OnlyRewardDistributorCanNotifyRewardAmount();
-    error StakingTokenAlreadyAdded();
 
     //// WRAPPED STRATEGY CURVE SWAPPER ////
     error OracleOutdated();
