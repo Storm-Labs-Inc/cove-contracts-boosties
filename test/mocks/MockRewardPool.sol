d69d03aa9a3ca452c35efd21dd6072080bd0b952
Jongseung (John) Lim <penandlim@gmail.com>
2023-12-05 12:40:59 -0500

    test: add unit tests for YearnStakingDelegate (#110)
    * test: add unit tests for YearnStakingDelegate
    
    * test: add fuzzing for execute
    
    * test: add fuzzing for swapAndLock test
    
    * test: add execute failed case

---
+++ test/mocks/MockRewardPool.sol
@@ -0,0 +1,15 @@
+pragma solidity ^0.8.18;
+
+import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract MockRewardPool {
+    address public immutable REWARD_TOKEN;
+
+    constructor(address rewardToken_) {
+        REWARD_TOKEN = rewardToken_;
+    }
+
+    function claim() public returns (uint256) {
+        IERC20(REWARD_TOKEN).transfer(msg.sender, IERC20(REWARD_TOKEN).balanceOf(address(this)));
+    }
+}
