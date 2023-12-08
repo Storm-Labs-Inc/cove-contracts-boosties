0cd2632567914f74206a3142bb476c1d638b2c89
Sunil Srivatsa <sunilsrivatsa@gmail.com>
2023-12-08 15:12:12 -0500

    chore: add missing natspec comments (#121)
    * chore: add missing natspec comments
    
    * fix: rogue slither warning

--- src/strategies/YearnGaugeStrategyBase.sol
+++ src/strategies/YearnGaugeStrategyBase.sol
@@ -8,18 +8,32 @@ import { SafeERC20, IERC20 } from "@openzeppelin/contracts/token/ERC20/utils/Saf
 import { IERC4626 } from "@openzeppelin/contracts/interfaces/IERC4626.sol";
 import { IYearnVaultV2 } from "src/interfaces/deps/yearn/veYFI/IYearnVaultV2.sol";
 
+/// @title YearnGaugeStrategyBase
+/// @notice Abstract base contract for Yearn gauge strategies, handling deposits and withdrawals to the
+/// YearnStakingDelegate.
 abstract contract YearnGaugeStrategyBase {
     // Libraries
     using SafeERC20 for IERC20;
 
     // Immutable storage variables
-    /* solhint-disable immutable-vars-naming */
+    // solhint-disable immutable-vars-naming
+    /// @notice Address of the YearnStakingDelegate contract
     address public immutable yearnStakingDelegate;
+    /// @notice Address of the dYFI token
     address public immutable dYfi;
+    /// @notice Address of the vault's underlying asset
     address public immutable vaultAsset;
+    /// @notice Address of the Yearn vault
     address public immutable vault;
-    /* solhint-enable immutable-vars-naming */
+    // solhint-enable immutable-vars-naming
 
+    /**
+     * @dev Sets the initial configuration of the strategy and approves the maximum amount of tokens to the
+     * YearnStakingDelegate.
+     * @param asset_ The address of the asset (gauge token).
+     * @param yearnStakingDelegate_ The address of the Yearn Staking Delegate.
+     * @param dYfi_ The address of the dYFI token.
+     */
     constructor(address asset_, address yearnStakingDelegate_, address dYfi_) {
         address vault_ = IERC4626(asset_).asset();
         // slither-disable-next-line uninitialized-local
@@ -50,10 +64,20 @@ abstract contract YearnGaugeStrategyBase {
         IERC20(vault_).forceApprove(asset_, type(uint256).max);
     }
 
+    /**
+     * @dev Internal function to deposit assets into the YearnStakingDelegate.
+     * @param asset The address of the asset to deposit.
+     * @param amount The amount of the asset to deposit.
+     */
     function _depositToYSD(address asset, uint256 amount) internal virtual {
         IYearnStakingDelegate(yearnStakingDelegate).deposit(asset, amount);
     }
 
+    /**
+     * @dev Internal function to withdraw assets from the YearnStakingDelegate.
+     * @param asset The address of the asset to withdraw.
+     * @param amount The amount of the asset to withdraw.
+     */
     function _withdrawFromYSD(address asset, uint256 amount) internal virtual {
         // Withdraw gauge from YSD which transfers to msg.sender
         IYearnStakingDelegate(yearnStakingDelegate).withdraw(asset, amount);
