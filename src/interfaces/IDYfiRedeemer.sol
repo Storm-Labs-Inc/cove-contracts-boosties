d74cb2025f751291554a8edddf5f0777f4dfe183
Jongseung (John) Lim <penandlim@gmail.com>
2023-12-10 12:42:11 -0500

    feat: add DYfiRedeemer contract (#127)
    * feat: add DYfiRedeemer contract
    
    * chore: rename test file
    
    * chore: split events into separate file
    
    * test: increase coverage
    
    * test: increase more coverage
    
    * chore: add comments and remove unnecessary checks
    
    * feat: add kill function
    
    * chore: clean up comments and simplify math
    
    * docs: add return statements

---
+++ src/interfaces/IDYfiRedeemer.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import { IFlashLoanRecipient } from "src/interfaces/deps/balancer/IFlashLoanRecipient.sol";
+import { IAccessControl } from "@openzeppelin/contracts/access/AccessControl.sol";
+import { IDYfiRedeemerEvents } from "src/interfaces/IDYfiRedeemerEvents.sol";
+
+interface IDYfiRedeemer is IFlashLoanRecipient, IAccessControl, IDYfiRedeemerEvents {
+    function slippage() external view returns (uint256);
+    function getLatestPrice() external view returns (uint256);
+    function minYfiRedeem(uint256 dYfiAmount) external view returns (uint256);
+    function currentYfiRedeem(uint256 dYfiAmount) external view returns (uint256);
+    function expectedMassRedeemReward(uint256 dYfiAmount) external view returns (uint256);
+    function massRedeem(address[] calldata accounts, uint256[] calldata dYfiAmounts) external;
+    function setSlippage(uint256 slippage) external;
+}
