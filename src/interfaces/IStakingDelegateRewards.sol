63f1dab5bee2566157fa57c55ca618130395544b
Jongseung Lim <penandlim@gmail.com>
2024-01-29 18:32:42 -0500

    chore: merge duplicate interface

--- src/interfaces/IStakingDelegateRewards.sol
+++ src/interfaces/IStakingDelegateRewards.sol
@@ -1,6 +1,9 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.18;
 
-interface IStakingDelegateRewards {
+import { IAccessControl } from "@openzeppelin/contracts/access/AccessControl.sol";
+
+interface IStakingDelegateRewards is IAccessControl {
+    function getReward(address stakingToken) external;
     function setRewardReceiver(address receiver) external;
 }
--- src/interfaces/deps/yearn/veYFI/IStakingDelegateRewards.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.18;
-
-import { IAccessControl } from "@openzeppelin/contracts/access/AccessControl.sol";
-
-interface IStakingDelegateRewards is IAccessControl {
-    function getReward(address stakingToken) external;
-}
