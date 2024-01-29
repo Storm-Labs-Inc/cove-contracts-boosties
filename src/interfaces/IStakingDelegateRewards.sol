4aec9240f89ea2c34d6407bdbe260d8de0cde36f
Jongseung Lim <penandlim@gmail.com>
2024-01-29 18:13:06 -0500

    chore: omit config change

--- src/interfaces/IStakingDelegateRewards.sol
+++ src/interfaces/IStakingDelegateRewards.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.18;
 
 interface IStakingDelegateRewards {
     function setRewardReceiver(address receiver) external;
--- src/interfaces/IYearn4626RouterExt.sol
+++ src/interfaces/IYearn4626RouterExt.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.18;
 
 import { IYearn4626Router } from "Yearn-ERC4626-Router/interfaces/IYearn4626Router.sol";
 import { IYearnVaultV2 } from "./deps/yearn/veYFI/IYearnVaultV2.sol";
