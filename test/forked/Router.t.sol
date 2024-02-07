dc311532a4de515bc18ef9d7b1ae8ba6f8eb23f0
lilPlumberBoy <dalymike102@gmail.com>
2024-02-07 13:18:57 -0500

    feat: added test for coverage

--- test/forked/Router.t.sol
+++ test/forked/Router.t.sol
@@ -10,6 +10,9 @@ import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 contract Router_ForkedTest is BaseTest {
     Yearn4626RouterExt public router;
 
+    event Log(string message);
+    event LogBytes(bytes data);
+
     address public user;
     uint256 public userPriv;
 
@@ -82,6 +85,66 @@ contract Router_ForkedTest is BaseTest {
 
         assertEq(IERC20(MAINNET_CURVE_ETH_YFI_LP_TOKEN).balanceOf(user), 0);
         assertEq(IERC20(MAINNET_ETH_YFI_GAUGE).balanceOf(user), 949_289_266_142_683_599);
+    }
+
+    function test_curveLpTokenToYearnGauge_revertWhen_insufficientShares() public {
+        uint256 depositAmount = 1 ether;
+        airdrop(IERC20(MAINNET_CURVE_ETH_YFI_LP_TOKEN), user, depositAmount);
+
+        // Generate a permit signature
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(
+            userPriv, // user's private key
+            keccak256(
+                abi.encodePacked(
+                    "\x19\x01", // EIP-712 encoding
+                    IERC20Permit(MAINNET_CURVE_ETH_YFI_LP_TOKEN).DOMAIN_SEPARATOR(),
+                    // Frontend should use deadline with enough buffer and with the correct nonce
+                    // keccak256(abi.encode(PERMIT_TYPEHASH, user, address(router), depositAmount,
+                    // sourceToken.nonces(user),
+                    // block.timestamp + 100_000))
+                    keccak256(abi.encode(PERMIT_TYPEHASH, user, address(router), depositAmount, 0, block.timestamp))
+                )
+            )
+        );
+
+        bytes[] memory data = new bytes[](6);
+        data[0] = abi.encodeWithSelector(
+            SelfPermit.selfPermit.selector, MAINNET_CURVE_ETH_YFI_LP_TOKEN, depositAmount, block.timestamp, v, r, s
+        );
+        data[1] = abi.encodeWithSelector(
+            PeripheryPayments.pullToken.selector, MAINNET_CURVE_ETH_YFI_LP_TOKEN, depositAmount, address(router)
+        );
+        data[2] = abi.encodeWithSelector(
+            PeripheryPayments.approve.selector, MAINNET_CURVE_ETH_YFI_LP_TOKEN, MAINNET_ETH_YFI_VAULT_V2, _MAX_UINT256
+        );
+        data[3] = abi.encodeWithSelector(
+            Yearn4626RouterExt.depositToVaultV2.selector,
+            MAINNET_ETH_YFI_VAULT_V2,
+            depositAmount,
+            address(router),
+            // When depositing into vaults, the shares may be less than the deposit amount
+            // For yearn v2 vaults, use pricePerShare to calculate the shares
+            // 1e18 * depositAmount / YearnVaultV2.pricePerShare()
+            // setting as depositAmount to show fail case
+            depositAmount
+        );
+        data[4] = abi.encodeWithSelector(
+            PeripheryPayments.approve.selector, MAINNET_ETH_YFI_VAULT_V2, MAINNET_ETH_YFI_GAUGE, _MAX_UINT256
+        );
+        data[5] = abi.encodeWithSelector(
+            Yearn4626RouterBase.deposit.selector,
+            MAINNET_ETH_YFI_GAUGE,
+            949_289_266_142_683_599,
+            user,
+            // Gauges return shares 1:1 with the deposit amount
+            949_289_266_142_683_599
+        );
+
+        // uniswap code doesn't allow for below
+        // vm.expectRevert(abi.encodeWithSelector(Yearn4626RouterExt.InsufficientShares.selector));
+        vm.expectRevert(); // have to use generic revert for now
+        vm.prank(user);
+        router.multicall(data);
     }
 
     function test_pullTokensWithPermit2() public {
