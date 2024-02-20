62689ba7f20eb9f5f57cd0137319dbec253d4b1a
Jongseung Lim <penandlim@gmail.com>
2024-02-20 16:15:24 -0500

    test: improve try catch for all errors

--- echidna/BaseRewardsGauge.t.sol
+++ echidna/BaseRewardsGauge.t.sol
@@ -13,41 +13,45 @@ import { Strings } from "@openzeppelin/contracts/utils/Strings.sol";
 /// @dev This contract is used to test the aditional properties of BaseRewardsGauge
 ///     along with CryticERC4626PropertyTests.
 contract BaseRewardsGauge_EchidnaTest is CryticERC4626PropertyTests {
-    BaseRewardsGauge public rewardsGauge;
-    TestERC20Token[8] public rewards;
+    BaseRewardsGauge private _rewardsGauge;
+    TestERC20Token[8] private _rewards;
 
     constructor() {
         TestERC20Token _asset = new TestERC20Token("Test Token", "TT", 18);
         BaseRewardsGauge gaugeImpl = new BaseRewardsGauge();
         // Initialize the gauge with the asset token
-        rewardsGauge = BaseRewardsGauge(Clones.clone(address(gaugeImpl)));
-        rewardsGauge.initialize(address(_asset), "");
+        _rewardsGauge = BaseRewardsGauge(Clones.clone(address(gaugeImpl)));
+        _rewardsGauge.initialize(address(_asset), "");
         // Initialize the rewards
         for (uint256 i = 0; i < 8; i++) {
-            rewards[i] = new TestERC20Token(
+            _rewards[i] = new TestERC20Token(
                 string.concat("Reward Token ", Strings.toString(i)), string.concat("RT", Strings.toString(i)), 18
             );
-            rewardsGauge.addReward(address(rewards[i]), address(this));
-            rewards[i].forceApproval(address(this), address(rewardsGauge), type(uint256).max);
+            _rewardsGauge.addReward(address(_rewards[i]), address(this));
+            _rewards[i].forceApproval(address(this), address(_rewardsGauge), type(uint256).max);
         }
         // Initialize CryticERC4626PropertyTests
-        initialize(address(rewardsGauge), address(_asset), false);
+        initialize(address(_rewardsGauge), address(_asset), false);
     }
 
     /// @notice Verify that the reward tokens are deposited and credited correctly
     function verify_depositRewardTokenProperties(uint256 rewardIndex, uint256 tokens) public {
         rewardIndex = clampBetween(rewardIndex, 0, 7);
         tokens = clampBetween(tokens, 1, type(uint104).max);
-        TestERC20Token reward = rewards[rewardIndex];
+        TestERC20Token reward = _rewards[rewardIndex];
         reward.mint(address(this), tokens);
-        uint256 balanceBefore = reward.balanceOf(address(rewardsGauge));
-        try rewardsGauge.depositRewardToken(address(reward), tokens) {
+        uint256 balanceBefore = reward.balanceOf(address(_rewardsGauge));
+        try _rewardsGauge.depositRewardToken(address(reward), tokens) {
             assertEq(
-                reward.balanceOf(address(rewardsGauge)),
+                reward.balanceOf(address(_rewardsGauge)),
                 balanceBefore + tokens,
                 "depositRewardToken() must credit the correct number of reward tokens to the gauge"
             );
-        } catch Error(string memory) {
+        } catch (bytes memory reason) {
+            // Ignore the case where the reward amount is too low
+            if (abi.decode(reason, (bytes4)) == BaseRewardsGauge.RewardAmountTooLow.selector) {
+                revert();
+            }
             assertWithMsg(false, "depositRewardToken() must not revert");
         }
     }
@@ -58,18 +62,18 @@ contract BaseRewardsGauge_EchidnaTest is CryticERC4626PropertyTests {
         uint256[8] memory balancesBefore;
         uint256[8] memory claimableRewards;
         for (uint256 i = 0; i < 8; i++) {
-            balancesBefore[i] = rewards[i].balanceOf(user);
-            claimableRewards[i] = rewardsGauge.claimableReward(user, address(rewards[i]));
+            balancesBefore[i] = _rewards[i].balanceOf(user);
+            claimableRewards[i] = _rewardsGauge.claimableReward(user, address(_rewards[i]));
         }
-        try rewardsGauge.claimRewards(user, address(0)) {
+        try _rewardsGauge.claimRewards(user, address(0)) {
             for (uint256 i = 0; i < 8; i++) {
                 assertEq(
-                    rewards[i].balanceOf(user),
+                    _rewards[i].balanceOf(user),
                     balancesBefore[i] + claimableRewards[i],
                     "claimRewards() must credit the correct number of reward tokens to the user"
                 );
             }
-        } catch Error(string memory) {
+        } catch (bytes memory reason) {
             assertWithMsg(false, "claimRewards() must not revert");
         }
     }
