c1f5b35a7cf53c129fda6d9c550272803f7d6f17
Sunil Srivatsa <sunilsrivatsa@gmail.com>
2024-02-22 15:56:28 -0500

    fix: reentrancy-events slither warnings

--- src/registries/CoveYearnGaugeFactory.sol
+++ src/registries/CoveYearnGaugeFactory.sol
@@ -127,8 +127,14 @@ contract CoveYearnGaugeFactory is AccessControl {
         address rewardForwarderImpl_ = rewardForwarderImpl;
         address treasuryMultisig_ = treasuryMultisig;
 
-        // Deploy and initialize the auto-compounding gauge
+        // Deploy both gauges
         BaseRewardsGauge coveStratGauge = BaseRewardsGauge(Clones.clone(baseRewardsGaugeImpl));
+        YSDRewardsGauge coveYsdGauge = YSDRewardsGauge(Clones.clone(ysdRewardsGaugeImpl));
+
+        // Emit the event
+        emit CoveGaugesDeployed(yearnGauge, coveYearnStrategy, address(coveStratGauge), address(coveYsdGauge));
+
+        // Initialize the auto-compounding gauge
         coveStratGauge.initialize(coveYearnStrategy);
         // Deploy and initialize the reward forwarder for the auto-compounding gauge
         {
@@ -148,8 +154,7 @@ contract CoveYearnGaugeFactory is AccessControl {
             coveStratGauge.renounceRole(_MANAGER_ROLE, address(this));
         }
 
-        // Deploy the non-auto-compounding gauge and initialize it
-        YSDRewardsGauge coveYsdGauge = YSDRewardsGauge(Clones.clone(ysdRewardsGaugeImpl));
+        // Initialize the non-auto-compounding gauge
         coveYsdGauge.initialize(yearnGauge, YEARN_STAKING_DELEGATE, coveYearnStrategy);
         // Deploy and initialize the reward forwarder for the non-auto-compounding gauge
         {
@@ -172,8 +177,6 @@ contract CoveYearnGaugeFactory is AccessControl {
             coveYsdGauge.renounceRole(DEFAULT_ADMIN_ROLE, address(this));
             coveYsdGauge.renounceRole(_MANAGER_ROLE, address(this));
         }
-        // Emit the event
-        emit CoveGaugesDeployed(yearnGauge, coveYearnStrategy, address(coveStratGauge), address(coveYsdGauge));
 
         // Save the gauge info
         supportedYearnGauges.push(yearnGauge);
