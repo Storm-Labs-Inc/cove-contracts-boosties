42f15d09285b93e9f875506cf5f436ea95ca9bf9
lilPlumberBoy <dalymike102@gmail.com>
2024-03-05 11:50:03 -0500

    feat: locked pragma for all contracts

--- src/DYFIRedeemer.sol
+++ src/DYFIRedeemer.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.18;
+pragma solidity 0.8.18;
 
 import { IFlashLoanProvider } from "src/interfaces/deps/balancer/IFlashLoanProvider.sol";
 import { IRedemption } from "src/interfaces/deps/yearn/veYFI/IRedemption.sol";
--- src/StakingDelegateRewards.sol
+++ src/StakingDelegateRewards.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.18;
+pragma solidity 0.8.18;
 
 import { AccessControl } from "@openzeppelin/contracts/access/AccessControl.sol";
 import { ReentrancyGuard } from "@openzeppelin/contracts/security/ReentrancyGuard.sol";
