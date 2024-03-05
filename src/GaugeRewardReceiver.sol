24c8394e02b93cd9a527dfdd120775813e448bfd
Michael Daly <97006234+lilPlumberBoy@users.noreply.github.com>
2024-03-05 17:17:29 -0500

    fix(QSP-COVE-16): unlocked Pragma (#243)
    * feat: locked pragma
    
    * feat: locked pragma for all contracts
    
    ---------
    
    Co-authored-by: Sunil Srivatsa <sunilsrivatsa@gmail.com>
    Co-authored-by: Jongseung (John) Lim <penandlim@gmail.com>

--- src/GaugeRewardReceiver.sol
+++ src/GaugeRewardReceiver.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.18;
+pragma solidity 0.8.18;
 
 import { Clone } from "lib/clones-with-immutable-args/src/Clone.sol";
 import { IGauge } from "src/interfaces/deps/yearn/veYFI/IGauge.sol";
--- src/rewards/MiniChefV3.sol
+++ src/rewards/MiniChefV3.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.18;
+pragma solidity 0.8.18;
 
 import { Multicall } from "@openzeppelin/contracts/utils/Multicall.sol";
 import { AccessControlEnumerable } from "@openzeppelin/contracts/access/AccessControlEnumerable.sol";
