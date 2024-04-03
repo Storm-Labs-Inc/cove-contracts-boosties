91829c53f6c09683d656d5fe278052254ee351a9
Sunil Srivatsa <sunilsrivatsa@gmail.com>
2024-04-02 20:10:03 -0400

    fix: update license in solidity files

--- src/interfaces/IYearn4626RouterExt.sol
+++ src/interfaces/IYearn4626RouterExt.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: BUSL-1.1
 pragma solidity ^0.8.18;
 
 import { IYearn4626Router } from "Yearn-ERC4626-Router/interfaces/IYearn4626Router.sol";
