b6aa76e35d99941a61b4b35ba1a5fbaf0bfc5293
Sunil Srivatsa <sunilsrivatsa@gmail.com>
2024-04-04 12:55:09 -0400

    chore: update license

--- test/utils/PermitSingature.t.sol
+++ test/utils/PermitSingature.t.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: BUSL-1.1
 pragma solidity ^0.8.18;
 
 import { YearnV3BaseTest, console } from "test/utils/YearnV3BaseTest.t.sol";
