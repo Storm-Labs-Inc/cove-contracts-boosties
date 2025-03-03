1b722e288847a4fb723b70cd8fd71d4ee209ad96
Michael Daly <97006234+lilPlumberBoy@users.noreply.github.com>
2025-03-03 13:23:38 -0500

    feat: staging master registry deploy (#349)
    * feat: staging master registry deploy
    
    * fix: added consts to utils
    
    * fix: updated deployer to be manager
    
    * chore: add Staging_MasterRegistry
    
    ---------
    
    Co-authored-by: Jongseung Lim <penandlim@gmail.com>

---
+++ script/DeployStagingMasterRegistry.s.sol
@@ -0,0 +1,56 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity 0.8.18;
+
+import { BaseDeployScript } from "script/BaseDeployScript.s.sol";
+import { DeployerFunctions, DefaultDeployerFunction, Deployer } from "generated/deployer/DeployerFunctions.g.sol";
+import { MasterRegistry } from "src/MasterRegistry.sol";
+import { AccessControlEnumerable } from "@openzeppelin/contracts/access/AccessControlEnumerable.sol";
+
+contract DeployStagingMasterRegistry is BaseDeployScript {
+    // Using generated functions
+    using DeployerFunctions for Deployer;
+    // Using default deployer function
+    using DefaultDeployerFunction for Deployer;
+
+    address public stagingMasterRegistry;
+
+    function deploy() public override {
+        require(MAINNET_COVE_DEPLOYER == msg.sender, "Sender must be mainnet deployer");
+        deployer.setAutoBroadcast(true);
+
+        // Deploy Staging Master Registry
+        stagingMasterRegistry = deployStagingMasterRegistry();
+
+        // Verify deployments
+        verifyPostDeploymentState();
+    }
+
+    function deployStagingMasterRegistry() public deployIfMissing("Staging_MasterRegistry") returns (address) {
+        return address(
+            deployer.deploy_MasterRegistry(
+                "Staging_MasterRegistry", COVE_STAGING_COMMUNITY_MULTISIG, MAINNET_COVE_DEPLOYER, options
+            )
+        );
+    }
+
+    function verifyPostDeploymentState() public view {
+        // Verify roles have been properly set for MasterRegistry
+        _verifyRole("Staging_MasterRegistry", DEFAULT_ADMIN_ROLE, COVE_STAGING_COMMUNITY_MULTISIG);
+        _verifyRole("Staging_MasterRegistry", MANAGER_ROLE, MAINNET_COVE_DEPLOYER);
+        _verifyRoleCount("Staging_MasterRegistry", DEFAULT_ADMIN_ROLE, 1);
+        _verifyRoleCount("Staging_MasterRegistry", MANAGER_ROLE, 2);
+    }
+
+    function _verifyRole(string memory contractName, bytes32 role, address user) internal view {
+        AccessControlEnumerable contractInstance = AccessControlEnumerable(deployer.getAddress(contractName));
+        require(contractInstance.hasRole(role, user), string.concat("Incorrect role for: ", contractName));
+    }
+
+    function _verifyRoleCount(string memory contractName, bytes32 role, uint256 count) internal view {
+        AccessControlEnumerable contractInstance = AccessControlEnumerable(deployer.getAddress(contractName));
+        require(
+            contractInstance.getRoleMemberCount(role) == count,
+            string.concat("Incorrect role count for: ", contractName)
+        );
+    }
+}
