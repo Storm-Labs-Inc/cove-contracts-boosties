933cfc8484114b6e82bb38d389b4118770ee874f
lilPlumberBoy <dalymike102@gmail.com>
2025-06-20 09:25:34 -0400

    feat: dYFIRedeemerV2

---
+++ script/DeployDYFIRedeemerV2.sol
@@ -0,0 +1,53 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity 0.8.18;
+
+import { BaseDeployScript } from "script/BaseDeployScript.s.sol";
+import { DeployerFunctions, DefaultDeployerFunction, Deployer } from "generated/deployer/DeployerFunctions.g.sol";
+import { AccessControlEnumerable } from "@openzeppelin/contracts/access/AccessControlEnumerable.sol";
+import { DYFIRedeemerV2 } from "src/DYFIRedeemerV2.sol";
+
+contract DeployDYFIRedeemerV2 is BaseDeployScript {
+    // Using generated functions
+    using DeployerFunctions for Deployer;
+    // Using default deployer function
+    using DefaultDeployerFunction for Deployer;
+
+    address public dYfiRedeemerV2;
+    address public admin;
+
+    function deploy() public override {
+        require(MAINNET_COVE_DEPLOYER == msg.sender, "Sender must be mainnet deployer");
+        deployer.setAutoBroadcast(true);
+
+        admin = MAINNET_COVE_COMMUNITY_MULTISIG;
+
+        // Deploy DYFIRedeemerV2
+        dYfiRedeemerV2 = deployDYFIRedeemerV2();
+
+        // Verify deployments
+        verifyPostDeploymentState();
+    }
+
+    function deployDYFIRedeemerV2() public deployIfMissing("DYFIRedeemerV2") returns (address) {
+        return address(deployer.deploy_DYFIRedeemerV2("DYFIRedeemerV2", admin, options));
+    }
+
+    function verifyPostDeploymentState() public view {
+        // Verify roles have been properly set for DYFIRedeemerV2
+        _verifyRole("DYFIRedeemerV2", DEFAULT_ADMIN_ROLE, admin);
+        _verifyRoleCount("DYFIRedeemerV2", DEFAULT_ADMIN_ROLE, 1);
+    }
+
+    function _verifyRole(string memory contractName, bytes32 role, address user) internal view {
+        AccessControlEnumerable contractInstance = AccessControlEnumerable(deployer.getAddress(contractName));
+        require(contractInstance.hasRole(role, user), string.concat("Incorrect role for: ", contractName));
+    }
+
+    function _verifyRoleCount(string memory contractName, bytes32 role, uint256 count) internal view {
+        AccessControlEnumerable contractInstance = AccessControlEnumerable(deployer.getAddress(contractName));
+        require(
+            contractInstance.getRoleMemberCount(role) == count,
+            string.concat("Incorrect role count for: ", contractName)
+        );
+    }
+}
